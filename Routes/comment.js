2518139439814e24af57c1975a5c55f6d1e89c0f
veena1vr <veenaluhaniwal.com>
2025-08-02 13:32:20 +0530

    ee

--- Routes/comment.js
+++ Routes/comment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const CommentController = require('../Controllers/commentController');
 const auth = require('../middleware/Authentication')
 
-router.post('/comment',auth, CommentController.addComment);
+router.post('/comment', CommentController.addComment);
 router.get('/comments/:videoId', CommentController.getCommentsByVideoId);
 
 module.exports = router;
